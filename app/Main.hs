e9f1427b7758619784f075ca15f3e02bf0096edb
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-11-21 14:01:07 +0000

    Color before branch based on what sort of ref it is

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,7 @@ import Data.ByteString (putStr)
 import Data.ByteString.Lazy (toStrict)
 import Data.Maybe (fromMaybe)
 import Control.Applicative ((<|>))
-import Data.String (fromString)
+import Data.String (fromString, IsString)
 import Data.Text (Text, strip, intercalate, stripPrefix, split, unpack)
 import Data.Text.Encoding (encodeUtf8, decodeUtf8')
 import Control.Monad.Trans.Class (lift)
@@ -21,6 +21,40 @@ import Text.Read (readMaybe)
 
 putTextUtf8 :: Text -> IO ()
 putTextUtf8 = Data.ByteString.putStr . encodeUtf8
+
+reset = "\ESC[0m"
+
+nfgRed = "\ESC[31m"
+fgBrightRed = "\ESC[31;1m"
+bgRed = "\ESC[41m"
+bgBrightRed = "\ESC[41;1m"
+
+fgGreen = "\ESC[32m"
+fgBrightGreen = "\ESC[32;1m"
+bgGreen = "\ESC[42m"
+bgBrightGreen = "\ESC[42;1m"
+
+fgYellow = "\ESC[33m"
+fgBrightYellow = "\ESC[33;1m"
+bgYellow = "\ESC[43m"
+bgBrightYellow = "\ESC[43;1m"
+
+fgBrightCyan = "\ESC[36;1m"
+
+-- This doesn't handle nested colors.  Best to use one of
+--
+-- * https://hackage.haskell.org/package/rainbow
+-- * https://hackage.haskell.org/package/ansi-terminal
+-- * https://hackage.haskell.org/package/terminal
+putColoredVT100 :: Colored -> IO ()
+putColoredVT100 = \case
+  Plain text -> putTextUtf8 text
+  Colored color colored -> case color of
+    Red -> Prelude.putStr fgBrightRed *> putColoredVT100 colored *> Prelude.putStr reset
+    Green -> Prelude.putStr fgBrightGreen *> putColoredVT100 colored *> Prelude.putStr reset
+    Yellow -> Prelude.putStr fgBrightYellow *> putColoredVT100 colored *> Prelude.putStr reset
+    Cyan -> Prelude.putStr fgBrightCyan *> putColoredVT100 colored *> Prelude.putStr reset
+  Join c1 c2 -> putColoredVT100 c1 *> putColoredVT100 c2
 
 tshow :: Show a => a -> Text
 tshow = fromString . show
@@ -39,8 +73,35 @@ run p = do
 
   pure (exitCode, stdout)
 
-data Before = At Text | Before Text
+data Before = At Colored | Before Colored
 
+data Color = Green | Red | Yellow | Cyan
+
+data Colored = Plain Text
+             | Colored Color Colored
+             | Join Colored Colored
+
+instance IsString Colored where
+  fromString = Plain . fromString
+
+instance Semigroup Colored where
+  (<>) = Join
+
+data RefType = Remote | Head | Tag
+
+refType :: Text -> Maybe (RefType, Text)
+refType ref =
+      match Remote "remotes/"
+  <|> match Head "heads/"
+  <|> match Tag "tags/"
+  where match rt s = fmap ((,) rt) (stripPrefix (fromString s) ref)
+
+colorRef :: RefType -> (Colored -> Colored)
+colorRef = Colored . \case { Head -> Green; Remote -> Red; Tag -> Yellow }
+-- Test case:
+--
+-- git describe --all --long
+-- tags/tag-f72b0d2-0-gf72b0d2
 before :: String -> ExceptT String IO (Maybe Before)
 before gitDir = do
   (exitCode, stdout) <- run (proc "git" ["-C", gitDir, "describe", "--all", "--long"])
@@ -48,14 +109,15 @@ before gitDir = do
   case exitCode of
     ExitSuccess -> case Prelude.reverse (split (== '-') stdout) of
       _rev:distanceT:refParts ->
-        let ref = intercalate (fromString "-") refParts
-            shortRef = fromMaybe ref (stripPrefix (fromString "remotes/") ref
-                                      <|> stripPrefix (fromString "heads/") ref)
+        let ref = intercalate (fromString "-") (reverse refParts)
+            shortRef = case refType ref of
+              Nothing -> Plain ref
+              Just (refType_, shortRef_) -> colorRef refType_ (Plain shortRef_)
 
         in case treadMaybe distanceT :: Maybe Int of
           Nothing -> throwE ("Couldn't read distance: " <> unpack distanceT)
           Just 0 -> pure (Just (At shortRef))
-          Just _ -> pure (Just (Before (shortRef <> fromString "-" <> distanceT)))
+          Just _ -> pure (Just (Before (shortRef <> fromString "-" <> Plain distanceT)))
       _ -> pure Nothing
     -- I don't know a way of distinguishing between the various error
     -- conditions
@@ -72,7 +134,7 @@ after gitDir = do
   case exitCode of
     ExitSuccess -> case Prelude.reverse (split (== '~') stdout) of
       distance:refParts ->
-        let ref = intercalate (fromString "~") refParts
+        let ref = intercalate (fromString "~") (reverse refParts)
             shortRef = fromMaybe ref (stripPrefix (fromString "remotes/") ref
                                       <|> stripPrefix (fromString "heads/") ref)
 
@@ -92,12 +154,14 @@ main = do
     beforeStr <- before gitDir
     afterStr  <- after gitDir
 
-    lift $ putTextUtf8 $ case (beforeStr, afterStr) of
+    let branch = Colored Cyan (Plain (fromString "ᛘ"))
+
+    lift $ putColoredVT100 $ case (beforeStr, afterStr) of
       (Nothing, Nothing) -> fromString ""
-      (Just (At b), _) -> fromString "ᛘ=" <> b
-      (Just (Before b), Nothing) -> b <> fromString "-ᛘ"
-      (Nothing, Just a) -> fromString "ᛘ-" <> a
-      (Just (Before b), Just a) -> b <> fromString "-ᛘ-" <> a
+      (Just (At b), _) -> branch <> fromString "=" <> b
+      (Just (Before b), Nothing) -> b <> fromString "-" <> branch
+      (Nothing, Just a) -> branch <> fromString "-" <> Plain a
+      (Just (Before b), Just a) -> b <> fromString "-" <> branch <> fromString "-" <> Plain a
 
   case r of
     Left l -> Prelude.putStrLn l
